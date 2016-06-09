6927f6919430aa3b44da3c23a7406eab8fcfc14d
Reactor13 <o.n.korchagin@gmail.com>
2016-06-09 23:00:48 +0300

    up

--- server/index.js
+++ server/index.js
@@ -34,8 +34,8 @@ var appServer = new http.Server(function(req,res)
 	var clientIP               = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 	var clientHost             = req.headers.host;
 	var clientOrigin           = req.headers.origin;
-	
-	if (clientOrigin == undefined || clientOrigin == null) {clientOrigin = '*'}
+		
+	if (clientOrigin == undefined || clientOrigin == null || clientOrigin == 'null') {clientOrigin = '*'}
 	console.log('[Server] New request from : ' + clientIP + ', host :' + clientHost + ', origin : ' + clientOrigin);
 	
 	if (urlParsed.pathname == '/')
